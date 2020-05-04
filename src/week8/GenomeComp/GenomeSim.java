aeaf515112d313329398cd3ab7e268fe79900eda
devop <ICHx@github.com>
2020-05-04 20:37:44 +0800

    4-may

--- src/week8/GenomeComp/GenomeSim.java
+++ src/week8/GenomeComp/GenomeSim.java
@@ -2,33 +2,36 @@ package week8.GenomeComp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
-public class Genomp {
-static char[] geneChar = {'A', 'C', 'G', 'T'};
-static int[][] genePair = {{10, -5, 0, -5}, {-5, 10, -5, 0}, {0, -5, 10, -5}, {-5, 0, -5, 10}};
+public class GenomeSim {
+//    private static Scanner sc = new Scanner(System.in);
+
+static final int[][] geneScorePair = {{10, -5, 0, -5}, {-5, 10, -5, 0}, {0, -5, 10, -5}, {-5, 0, -5, 10}};
 
 
-public static String DNA_A = "GAATC";
-public static String DNA_B = "CATAC";
+public static String DNA_A = "GAATC"; //columns
+public static String DNA_B = "AATTC"; //rows
+//public static String DNA_B = "CATAC"; //rows
 
 public static void main(String[] args) {
-    //get 2 strings
+////      get 2 strings
+//    DNA_A=sc.nextLine();
+//    DNA_B=sc.nextLine();
     
     // create dot plot matrix
     int[][] dotPlot;
-    dotPlot = new int[DNA_A.length() + 1][DNA_B.length() + 1];
-    
     byte[][] dotLink;
-    dotLink = new byte[DNA_A.length() + 1][DNA_B.length() + 1];
-    /*
-     * 0b000001 : right down link
-     * 0b000100 : down right link
-     * 0b010000 : diagonal   link
-     * */
+    
+    int tableHorizonLength = DNA_A.length() + 1;
+    int tableVerticalLength = DNA_B.length() + 1;
+    
+    dotPlot = new int[tableVerticalLength][tableHorizonLength];
+    dotLink = new byte[tableVerticalLength][tableHorizonLength];
     
     
-    for (int[] ints : dotPlot) {
-        Arrays.fill(ints, Integer.MIN_VALUE);
+    for (int[] line : dotPlot) {
+        Arrays.fill(line, Integer.MIN_VALUE);
     }
     
     dotPlot[0][0] = 0;
@@ -48,19 +51,24 @@ public static void main(String[] args) {
     
     
     int i = 0, j = 0;
-//            initialize 0th row
-    for (int k = j + 1; k < dotPlot[0].length; k++) {
+//      initialize 0th row (left to right)
+    
+    for (int k = j + 1; k < tableHorizonLength; k++) {
         dotPlot[i][k] = Math.max(dotPlot[i][k - 1] - 4, dotPlot[i][k]);
+        dotLink[i][k] += 0b1;
     }
-//            initialize 0th col
-    for (int k = i + 1; k < dotPlot.length; k++) {
+
+
+//      initialize 0th col (up to down)
+    for (int k = i + 1; k < tableVerticalLength; k++) {
         dotPlot[k][j] = Math.max(dotPlot[k - 1][j] - 4, dotPlot[k][j]);
+        dotLink[k][j] += 0b100;
     }
 
 
 //      following boxes
-    for (i = 1; i < dotPlot.length; i++) {
-        for (j = 1; j < dotPlot[0].length; j++) {
+    for (i = 1; i < tableVerticalLength; i++) {
+        for (j = 1; j < tableHorizonLength; j++) {
             
             
             int best;
@@ -68,30 +76,33 @@ public static void main(String[] args) {
             
             Route1:
             {
-                // go right
+                // go right (from left)
                 right = dotPlot[i][j - 1] - 4;
             }
             
             Route2:
             {
-                // go down
+                // go down (from up)
                 down = dotPlot[i - 1][j] - 4;
             }
             
             Route3:
             {
-                dg = dotPlot[i - 1][j - 1] + genePair[geneB[i]][geneA[j]];
+                dg = dotPlot[i - 1][j - 1] + geneScorePair[geneB[i]][geneA[j]];
             }
             
             SelectBest:
             {
                 best = Math.max(right, Math.max(down, dg));
+                //! establishing links(in-edges)
                 
-                // establishing links(in edges)
-                // right arrow
-                // down  arrow
-                // diag  arrow
+                // it is easier to perform path add than instead of inside comparison if-statements
                 
+                /*
+                 * 0b000001 : right link
+                 * 0b000100 : down link
+                 * 0b010000 : diagonal link
+                 * */
                 if (best == right) {
                     dotLink[i][j] += 0b1;
                 }
@@ -104,21 +115,31 @@ public static void main(String[] args) {
             }
             dotPlot[i][j] = best;
         }
-    }//end of scoring
+    }//end of scoring system
     
-    assert false;
     
-    //start of path choosing, from dp[m][n]
+    //start of path tracing, from dp[m][n]
     int m = DNA_B.length(), n = DNA_A.length();
     //initial m=5 n=5
     ArrayList<Integer> subPath = new ArrayList<>();
     ArrayList<Character> genMatchI = new ArrayList<>();
     ArrayList<Character> genMatchII = new ArrayList<>();
     
-    //everything are in reverse direction here
-    while (n != 0 || m != 0) {
-        char charA = DNA_A.charAt(n - 1); //offset start from zero
-        char charB = DNA_B.charAt(m - 1);
+    //in reverse traversal
+    char charA, charB;
+    while ((n | m) != 0) { //end at (0,0)
+        
+        if (m != 0) {
+            charB = DNA_B.charAt(m - 1);
+        } else {
+            charB = '-';
+        }
+        
+        if (n != 0) {
+            charA = DNA_A.charAt(n - 1); //offset start from zero
+        } else {
+            charA = '-';
+        }
         
         if ((dotLink[m][n] & 16) != 0) { //dg
             //append SB
@@ -145,12 +166,23 @@ public static void main(String[] args) {
             //continue traversal
 //            m -= 1;
             n -= 1;
-            continue;
         }
         
         
-    }
+    }//end of path tracing
     
+    
+    //display results
+    System.out.println("Original");
+    System.out.println("gene A: " + DNA_A);
+    System.out.println("gene B: " + DNA_B);
+    
+    Collections.reverse(genMatchI);
+    Collections.reverse(genMatchII);
+    System.out.println("\nMatched\n=====================\n");
+    System.out.println("gene A: " + genMatchI.toString());
+    System.out.println("gene B: " + genMatchII.toString());
+    System.out.println("Score: " + dotPlot[tableVerticalLength - 1][tableHorizonLength - 1]);
     
 }//end of main
 
